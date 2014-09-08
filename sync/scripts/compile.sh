50425188f668934a7ad1214b2a5ad45950a39d39
Federico Voges <federico@puppetlabs.com>
2014-09-08 21:32:42 +0100

    yum install -qy is not the same as yum install -q -y. oops

--- sync/scripts/compile.sh
+++ sync/scripts/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -qy haproxy
+yum install -q -y haproxy
 cp /vagrant/files/haproxy.cfg /etc/haproxy/
 chkconfig haproxy on
 service haproxy start
--- sync/scripts/provision.sh
+++ sync/scripts/provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -qy vim-enhanced bash-completion ccze colordiff curl git htop lftp lynx mc mutt psmisc rsync sysstat telnet wget mercurial tree avahi avahi-tools nss-mdns
+yum install -q -y vim-enhanced bash-completion ccze colordiff curl git htop lftp lynx mc mutt psmisc rsync sysstat telnet wget mercurial tree avahi avahi-tools nss-mdns
 
 /vagrant/scripts/rcfiles.sh
 sudo -nu vagrant -i /vagrant/scripts/rcfiles.sh
