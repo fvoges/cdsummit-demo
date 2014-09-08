03a282817453aa78f1950fb2bd2f52132630ef64
Federico Voges <federico@puppetlabs.com>
2014-09-08 21:30:20 +0100

    Added PATH statement to ensure that we use PE. Installs hiera.yaml, restarts puppet.

--- sync/scripts/master.sh
+++ sync/scripts/master.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+PATH="/usr/local/bin:$PATH"
+
 tar -C /tmp -xf /vagrant/pe-latest.tar.gz
 cd /tmp/puppet-enterprise-*
 ./puppet-enterprise-installer -a /vagrant/answerfiles/all-in-one.txt
+
 puppet config --section master set environmentpath  '$confdir/environments'
 puppet config --section master set default_manifest '$confdir/manifests'
 puppet config --section master set basemodulepath '$confdir/modules:/opt/puppet/share/puppet/modules'
+
+cp -f /vagrant/files/hiera.yaml /etc/puppetlabs/puppet/hiera.yaml
+
+service pe-httpd restart
 
 puppet module install puppetlabs-vcsrepo
 puppet module install jfryman-nginx
